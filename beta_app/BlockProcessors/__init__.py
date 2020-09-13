3dc36e8077997b9a7ba319d1abcd828803eef2bc
Andrew <59933938+andrewcampagnagit@users.noreply.github.com>
2020-09-12 20:11:43 -0500

    Add files via upload

---
+++ beta_app/BlockProcessors/__init__.py
@@ -0,0 +1,9 @@
+## BlockProcessors module version beta-1.0 ##
+
+"""
+
+Inventory:
+
+- kubectlblock
+For processing Kubernetes cloud deployments using kubectl. Use the command
+``dolphin explain blockprocessor.kubectlblock`` for usage info.
