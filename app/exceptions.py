4b4a34dde4c490d9ad4ac22f00a6e029c1b76382
Andrew Campagna <andrewcampagna@Andrews-Air-2.lan>
2020-09-23 00:11:55 -0500

    custom exceptions

---
+++ app/exceptions.py
@@ -0,0 +1,18 @@
+INSTRUCTION_DL_FUNC_NOTFOUND = """The instructions download function was not found...
+
+Please specify a function <function> <file path/url>
+
+NAME				OPTION				EXPECTS				DESCRIPTION
+
+From file			--file/-f			File path			Downloads from file path
+From [GET]			--GET/-G			URL      			Downloads from URL
+"""
+
+DOLPHIN_COMMAND_FUNC_NOTFOUND = """The dolphin command function was not found...
+
+Please specify a dolphin function <function> <download function>
+
+NAME				OPTION				EXPECTS				DESCRIPTION
+
+Deploy				deploy/-d			Instructions file	Deploys new application from instructions file
+"""
